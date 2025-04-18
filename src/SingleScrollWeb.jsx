751a41287426e97696696a12fc7861be1224905c
sanatVibhor <sanatvibhor2@gmail.com>
2025-04-18 22:13:15 +0530

    4th commit

--- src/SingleScrollWeb.jsx
+++ src/SingleScrollWeb.jsx
@@ -107,7 +107,7 @@ const SingleScrollWebsite = () => {
               <div className="relative h-64 sm:h-72 md:h-96 overflow-hidden rounded-lg shadow-xl">
                 <div className="absolute inset-0 bg-gray-300">
                   
-                  <img src="../photos/image_1.jpg" alt="Children learning in Ranchi center" className="w-full h-full object-cover" />
+                  <img src="../photos/image_1.JPG" alt="Children learning in Ranchi center" className="w-full h-full object-cover" />
                 </div>
               </div>
             </div>
